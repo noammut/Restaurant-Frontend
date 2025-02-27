cb62a9949192408071c2829825655baa83fe7f17
Tamkinon <tamirbendavid95@gmail.com>
2025-02-27 08:49:40 +0200

    added basic dashboard

--- admin/script.js
+++ admin/script.js
@@ -1,3 +1,24 @@
+async function login() {
+    // const username = document.getElementById('username').value;
+    // const password = document.getElementById('password').value;
+    // try {
+    //     await axios.post('http://127.0.0.1:5000/login', {
+    //         username: username,
+    //         password: password,
+    //     });
+        
+    //     // Clear form fields
+    //     document.getElementById('username').value = '';
+    //     document.getElementById('password').value = '';
+        
+    // } catch (error) {
+    //     alert('Login Failed');
+    //     console.error('Login error: Invalid credentials', error);
+    // }
+
+    window.location.href = 'dashboard.html'
+}
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-options");
     menu.style.display = menu.style.display === "block" ? "none" : "block";
